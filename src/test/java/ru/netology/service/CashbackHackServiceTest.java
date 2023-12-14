8b1e427f1b008f812f3d238ad4d9bb37023db6b6
leily <leily@example.com>
2023-12-14 17:24:09 +0300

    Junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn200() {
+        int actual = service.remain(800);
+        int expected = 200;
+        Assert.assertEquals(expected, actual);
+    }
+}
